46867e90f9f108a483de78f4215b0df48d2341a5
AKASH <itsmeakashsarkar@gmail.com>
2026-07-23 12:38:28 +0530

    Update hotspot.py

--- app/models/hotspot.py
+++ app/models/hotspot.py
@@ -6,7 +6,6 @@ datetime.now(timezone.utc)
 
 from sqlalchemy import DateTime, Enum, Float, Integer
 from sqlalchemy.orm import Mapped, mapped_column
-
 from app.database import Base
 from app.utils.constants import HotspotStatus
 
@@ -26,6 +25,7 @@ class Hotspot(Base):
         Enum(HotspotStatus, name="hotspot_status"), nullable=False, default=HotspotStatus.ACTIVE
     )
 
+    
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc), nullable=False, index=True)
 
     def __repr__(self) -> str:
